d320021ebd994c05090a50f6a47eadf4cae78dd9
joshuabradley012 <joshuabradley012@gmail.com>
2022-02-07 14:57:36 -0800

    Updated mobile

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <section className="page">
+    <section className="section">
       <div className="container">
         <div className={styles.hero}>
           <h1 className={styles.title}>Bakersfield&rsquo;s <br className="d-none d-md-inline"/>Best Chiropractor</h1>
-          <p className={styles.heroCaption}>Bradley Chiropractic Nutrition Center has made a name for itself by helping thousands of patients get out of pain. With a doctor and staff who authentically care about your well-being, we look forward to meeting you. Call today to start on the road towards better health.</p>
+          <p className={styles.caption}>Bradley Chiropractic Nutrition Center has made a name for itself by helping thousands of patients get out of pain. With a doctor and staff who authentically care about your well-being, we look forward to meeting you. Call today to start on the road towards better health.</p>
           <div className={styles.cta}>
             <Link href="/contact">
               <a className="btn">Schedule Now</a>
