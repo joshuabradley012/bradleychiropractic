3319e3f6bfcd079d2a7f829594bb60ff290289a3
joshuabradley012 <joshuabradley012@gmail.com>
2022-11-11 14:21:45 -0800

    Date test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const [hours, setHours] = useState(new Date().getHours());
+
+  useEffect(() => {
+    setHours(new Date().getHours());
+  }, [hours]);
+
   return (
-    <Html lang="en">
+    <Html lang="en" className={hours}>
       <Head />
       <body>
         <Main />
