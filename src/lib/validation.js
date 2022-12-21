7a4f8a6371f36728aece479bb907bf5cbd6107a0
joshuabradley012 <joshuabradley012@gmail.com>
2022-12-21 10:04:15 -0800

    Finished form

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const emailValidation = yup.string().email("Invalid email").required("Email required");
 
-export const subscriberSchema = yup.object().shape({
+export const subscriberSchema = yup.object({
   email: emailValidation,
 });
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/main.scss'
+import 'react-dates/initialize';
 import Layout from '@/components/layout';
 
 function App({ Component, pageProps }) {
