3747fe767d3f8a99abb62bbcef78ac54415ecfd2
joshuabradley012 <joshuabradley012@gmail.com>
2022-12-22 18:10:20 -0800

    New meta description

--- src/content/home.js
+++ src/content/home.js
@@ -8,7 +8,7 @@ import teamImage from '@/public/adjustment-room.png';
 
 const content = {
   seoTitle: 'Bradley Chiropractic Nutrition Center | Bakersfield Chiropractor',
-  seoDescription: 'Dr. Bradley is passionate about giving the best chiropractic care possible by using many methods; including spinal decompressions, exercise, and nutrition.',
+  seoDescription: 'Dr. Bradley provides the best chiropractic care possible by using a holistic approach to chiropractic medicine, including spinal decompressions, exercise, and nutrition.',
 
   hero: {
     heroImage,
