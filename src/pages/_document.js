27332faea9a1d06cd037688cd11af8dbce92f03d
Vann Bryan Galleta <vannbryan22@gmail.com>
2023-11-01 06:24:50 +0800

    fix:can't use script on _document file

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -8,12 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script 
-          src="https://cdn.reviewwave.com/js/chat_embed.js" 
-          strategy="lazyOnload"
-          type="text/javascript" 
-          data-id="4b5e2fe976efbc"
-        />
       </body>
     </Html>
   );
