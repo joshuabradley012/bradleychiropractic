bca21155f96c79cbe163342a89031c7d78e54ff7
Vann Bryan Galleta <vannbryan22@gmail.com>
2023-11-01 05:00:54 +0800

    feature: Embed Review Wave’s chat widget

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script 
+          src="https://cdn.reviewwave.com/js/chat_embed.js" 
+          strategy="lazyOnload"
+          type="text/javascript" 
+          data-id="4b5e2fe976efbc"
+        />
       </body>
     </Html>
   );
