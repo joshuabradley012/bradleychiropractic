739a03da565a174a2ac384677ab0a1b9f80e7a97
Vann Bryan Galleta <vannbryan22@gmail.com>
2023-11-01 04:38:42 +0800

    feature:Update the /schedule page to use Review Wave’s form

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -1,20 +1,32 @@
-import { useEffect, useRef } from 'react';
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
-const useScript = (url, dataId) => {
+export default function useScript(url, dataId, reloadOnRouteChange = false) {
+  const router = useRouter();
   const ref = useRef();
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = url;
-    script.setAttribute('data-id', dataId);
-    ref.current.appendChild(script);
+    const script = document.createElement("script");
+    script.setAttribute("src", url);
+    script.setAttribute("async", true);
+    script.setAttribute("data-id", dataId);
+    ref.current?.appendChild(script);
+
+    // Needed for cleaning residue left by the external script that can only be removed by reloading the page
+    const onRouterChange = (newPath) => {
+      window.location.href = router.basePath + newPath;
+    };
+    if (reloadOnRouteChange) {
+      router.events.on("routeChangeStart", onRouterChange);
+    }
 
     return () => {
-      ref.current.removeChild(script);
+      if (reloadOnRouteChange) {
+        router.events.off("routeChangeStart", onRouterChange);
+      }
+      ref.current?.removeChild(script);
     };
-  }, [url]);
+  }, [router, url]);
 
   return ref;
-};
-
-export default useScript;
+}
